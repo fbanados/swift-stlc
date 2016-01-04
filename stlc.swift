2ab8aa4090ba88e9bd517e7bfa3b49a75963b590
Felipe Bañados Schwerter <fbanados@cs.ubc.ca>
2016-01-04 10:58:02 -0800

    Fixed bugs in type equality and in typechecking of functions (T-Fun/T-Abs).

--- stlc.swift
+++ stlc.swift
@@ -2,51 +2,51 @@
 //: Playground - noun: a place where people can play
 
 //: LC : Implemements a minimal simply-typed lambda calculus with unit, ints and addition.
+//: In this version, we use nil instead of exceptions to avoid polluting the type signatures.
 
 var Binops = [
     "+": {(a: Int,b: Int) -> Int in
-        return a + b} , //Lambda syntax in swift.
- ]
+        return a + b} , //Lambda syntax in swift: lambda x : T1 . e : T1 -> T2 is {(x: T1) -> T2 in return e}
+]
 
 typealias Id = String
 
 indirect enum Type{
     case Unit
     case Int
-    case Fun([Type], Type)
+    case Fun([Type], Type)// [Type] = listof Type
 }
 
-func ==(a:Type?, b:Type?) -> Bool {
-    if let ta = a {
-        switch ta {
+// Equality of Types.
+extension Type: Equatable { }
+func ==(a:Type, b:Type) -> Bool {
+        switch a {
         case Type.Unit:
-            if let tb = b{
-                switch tb{
+                switch b{
                 case Type.Unit:
                     return true
                 default:
                     return false
-                }}
+                }
         case Type.Int:
-            if let tb = b{
-                switch tb{
+                switch b{
                 case Type.Int:
                     return true
                 default:
                     return false
-                }}
+                }
         case let Type.Fun(adom,arng):
-            if let tb = b{
-                switch tb{
+                switch b{
                 case let Type.Fun(bdom, brng):
-                    return zip(adom,bdom).reduce(true, combine: {(acc:Bool,dom:(a:Type,b:Type)) -> Bool in acc && a == b}) && arng == brng
+                    return zip(adom,bdom).reduce(true, combine: {(acc:Bool,dom:(a:Type,b:Type)) -> Bool in acc && dom.a == dom.b}) && arng == brng
                 default:
                     return false 
-                }}
-        }}
-    return false 
+                }
+        }
 }
 
+
+//equivalent to #lang plai's define-type.
 indirect enum Expr{ //indirect keyword is required for the recursion.
     case Unit
     case Lambda([(Id, Type)], Expr) //Multiargument lambda.
@@ -56,6 +56,7 @@ indirect enum Expr{ //indirect keyword is required for the recursion.
     case Ident(Id)
 }
 
+//definition of substitution function.
 func subst(e:Expr, x:Id, v:Expr) -> Expr{
     switch e{
     case .Unit:
@@ -81,10 +82,13 @@ func subst(e:Expr, x:Id, v:Expr) -> Expr{
     }
 }
 
+//Helper function, mimics scheme's andmap.
 func andmap<T>(list: [T], check:(T) -> Bool) -> Bool{
     return list.reduce(true, combine: {(acc: Bool, element:T) -> Bool in acc && check(element)})
 }
 
+//helper function to create a lambda wrapper for the combine function that also accepts nil for arguments.
+//useful for fold (in swift terms, reduce, which requires a "combine" lambda)
 func combine_opt<T,U>(combine: (T, U) -> T?) -> ((T?, U?) -> T?){
     return {(acc: T?, v: U?) -> T? in
         if let accv = acc {
@@ -96,6 +100,7 @@ func combine_opt<T,U>(combine: (T, U) -> T?) -> ((T?, U?) -> T?){
     }
 }
 
+//Evaluator. Mimics a big-step semantics.
 func eval(e: Expr) -> Expr?{
     switch e{
     case .Unit:
@@ -142,6 +147,7 @@ func eval(e: Expr) -> Expr?{
     return nil 
 }
 
+//an environment is a dictionary from identifiers to types.
 typealias Env = [Id:Type]
 
 let emptyEnv: Env = [Id:Type]()
@@ -163,6 +169,7 @@ func typecheck(e: Expr, env: Env) -> Type? {
     case let Expr.Ident(x):
         return env[x]
     case let Expr.Binop(id,op1,op2):
+        //for now, this case is assuming that all binops are Int x Int -> Int
         if (typecheck(op1,env: env) == Type.Int) && (typecheck(op2, env: env) == Type.Int){
                         if (Binops[id] != nil){
                             return Type.Int
@@ -170,7 +177,8 @@ func typecheck(e: Expr, env: Env) -> Type? {
         }
         return nil
     case let Expr.Lambda(args, body):
-        return typecheck(body, env: extend_env(env,with:args))
+        return Type.Fun(args.map({(arg:(x: Id, T: Type)) -> Type in arg.T}),
+                        typecheck(body, env: extend_env(env,with:args))!)
     case let Expr.App(f, args):
         if let tf = typecheck(f, env: env) {
             switch tf{
@@ -192,3 +200,31 @@ func typecheck(e: Expr, env: Env) -> Type? {
     }
     return nil 
 }
+
+func test<T: Equatable>(value:T?,expect:T?){
+    if let v=value{
+        if let x=expect{
+            if x==v{
+                print("Test passed for \(value)")
+            } else {
+                print("ERROR: Expected \(x), Got \(v)")
+            }
+        } else {
+            print("ERROR: Expected nil, Got \(v)")
+        }
+    } else {
+        if let x=expect{
+            print("ERROR: Expected \(x), got nil")
+        } else {
+            print("Test passed for nil")
+        }
+    }
+}
+
+print(typecheck(Expr.Lambda([("x", Type.Unit)], Expr.Ident("x") ), env: emptyEnv))
+print(Type.Fun([Type.Unit], Type.Unit))
+print(Type.Unit == Type.Unit)
+test(
+    typecheck(Expr.Lambda([("x", Type.Unit)], Expr.Ident("x") ), env: emptyEnv),
+    expect: Type.Fun([Type.Unit],Type.Unit)
+    )
